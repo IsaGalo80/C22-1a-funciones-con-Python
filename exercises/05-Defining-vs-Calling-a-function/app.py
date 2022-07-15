540fdd5c573d6222291071e75a916b208335bdad
IsaGalo80 <isagalo1980@outlook.com>
2022-07-15 15:08:05 +0000

    falta

--- exercises/05-Defining-vs-Calling-a-function/app.py
+++ exercises/05-Defining-vs-Calling-a-function/app.py
@@ -1,11 +1,6 @@
 # Define the function called "multi" that expects 2 parameters:
-def multi(num1,num2):
-# don't edit anything below this line
- return_value =num1 * num2
-        return return_value 
-print(return_value(7,53812212))
-
-
-
-
-
+def multi(number1,number2):
+# don't edit anything below this lin
+ return number1 * number2
+return_value = multi(7,53812212)
+print(return_value)
